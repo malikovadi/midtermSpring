8662b28a4decfcd7a4971207f2b1796ba66610e1
malikovadi <malikovadi21@gmail.com>
2024-05-12 12:28:03 +0600

    jwt and refresh tokens

--- src/main/java/com/kg/alatoo/midtermSpring/dto/authorization/JwtTokenDTO.java
+++ src/main/java/com/kg/alatoo/midtermSpring/dto/authorization/JwtTokenDTO.java
@@ -1,2 +1,15 @@
-package com.kg.alatoo.midtermSpring.dto.authorization;public class JwtTokenDTO {
+package com.kg.alatoo.midtermSpring.dto.authorization;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JwtTokenDTO {
+    private String accessToken;
+    private String token;
 }
--- src/main/java/com/kg/alatoo/midtermSpring/repositories/RefreshTokenRepository.java
+++ src/main/java/com/kg/alatoo/midtermSpring/repositories/RefreshTokenRepository.java
@@ -1,2 +1,12 @@
-package com.kg.alatoo.midtermSpring.repositories;public interface RefreshTokenRepository {
+package com.kg.alatoo.midtermSpring.repositories;
+
+import com.kg.alatoo.midtermSpring.entities.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Integer> {
+    Optional<RefreshToken> findByToken(String token);
 }
