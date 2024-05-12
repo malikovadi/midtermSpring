665d9933bca65c894c499d76c7361b69a7a06532
malikovadi <malikovadi21@gmail.com>
2024-05-12 12:33:26 +0600

    expiration time for access token

--- src/main/java/com/kg/alatoo/midtermSpring/services/JwtService.java
+++ src/main/java/com/kg/alatoo/midtermSpring/services/JwtService.java
@@ -80,7 +80,7 @@ public class JwtService {
                 .claims(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 2))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 20))
                 .signWith(getSignKey()).compact();
 
     }
