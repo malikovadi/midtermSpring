9ee0c48f13c2231a03f581a3d304deae3c8a3608
malikovadi <malikovadi21@gmail.com>
2024-05-12 12:24:14 +0600

    traditional and social authentication methods

--- src/main/java/com/kg/alatoo/midtermSpring/controllers/AuthController.java
+++ src/main/java/com/kg/alatoo/midtermSpring/controllers/AuthController.java
@@ -1,2 +1,72 @@
-package com.kg.alatoo.midtermSpring.controllers;public class AuthController {
+package com.kg.alatoo.midtermSpring.controllers;
+
+import com.kg.alatoo.midtermSpring.dto.UserDTO;
+import com.kg.alatoo.midtermSpring.dto.authorization.*;
+import com.kg.alatoo.midtermSpring.entities.RefreshToken;
+import com.kg.alatoo.midtermSpring.services.JwtService;
+import com.kg.alatoo.midtermSpring.services.RefreshTokenService;
+import com.kg.alatoo.midtermSpring.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+@Slf4j
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public UserDTO register(@Validated @RequestBody RegistrationDTO registrationDTO){
+        return userService.register(registrationDTO);
+    }
+
+    @PostMapping("/login")
+    public JwtTokenDTO AuthenticateAndGetToken(@RequestBody AuthLoginDTO authRequestDTO){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequestDTO.getUsername(), authRequestDTO.getPassword()));
+        if(authentication.isAuthenticated()){
+            RefreshToken refreshToken = refreshTokenService.createRefreshToken(authRequestDTO.getUsername());
+            return JwtTokenDTO.builder()
+                    .accessToken(jwtService.GenerateToken(authRequestDTO.getUsername()))
+                    .token(refreshToken.getToken())
+                    .build();
+
+        } else {
+            throw new UsernameNotFoundException("invalid user request..!!");
+        }
+    }
+    @PostMapping("/refreshToken")
+    public JwtTokenDTO refreshToken(@RequestBody RefreshTokenRequestDTO refreshTokenRequestDTO){
+        return refreshTokenService.findByToken(refreshTokenRequestDTO.getToken())
+                .map(refreshTokenService::verifyExpiration)
+                .map(RefreshToken::getUser)
+                .map(userInfo -> {
+                    String accessToken = jwtService.GenerateToken(userInfo.getUsername());
+                    return JwtTokenDTO.builder()
+                            .accessToken(accessToken)
+                            .token(refreshTokenRequestDTO.getToken()).build();
+                }).orElseThrow(() ->new RuntimeException("Refresh Token is not in DB..!!"));
+    }
+
+    @GetMapping
+    public Object getCurrentUser(){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof CustomUserDetails userDetails) {
+            return new UserDTO(userDetails.user());
+        }
+        return principal;
+    }
 }
--- src/main/java/com/kg/alatoo/midtermSpring/dto/authorization/AuthLoginDTO.java
+++ src/main/java/com/kg/alatoo/midtermSpring/dto/authorization/AuthLoginDTO.java
@@ -1,2 +1,15 @@
-package com.kg.alatoo.midtermSpring.dto.authorization;public class AuthLoginDTO {
+package com.kg.alatoo.midtermSpring.dto.authorization;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthLoginDTO {
+    private String username;
+    private String password;
 }
